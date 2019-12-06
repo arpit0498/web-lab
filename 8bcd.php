de893ff0f7ab52babe329415b1b946953458fa15
arpit0408 <guptakhiladi89@gmail.com>
2019-12-06 12:14:15 +0530

    lab

--- 8bcd.php
+++ 8bcd.php
@@ -5,45 +5,54 @@ $m=count($a);
 $n=count($a[2]);
 $p=count($b);
 $q=count($b[2]);
-echo "the first matrix :"."<br/>";
-for ($row = 0; $row < $m; $row++) {
-for ($col = 0; $col < $n; $col++)
-echo " ".$a[$row][$col];
-echo "<br/>";
-}
-echo "the second matrix :"."<br/>";
-for ($row = 0; $row < $p; $row++) {
-for ($col = 0; $col < $q; $col++)
-echo " ".$b[$row][$col];
-echo "<br/>";
-}
-echo "the transpose for the first matrix is:"."<br/>";
-for ($row = 0; $row < $m; $row++) {
-for ($col = 0; $col < $n; $col++)
-echo " ".$a[$col][$row];
-echo "<br/>";
-}
-if(($m===$p) and ($n===$q)) {
-echo "the addition of matrices is:"."<br/>";
-for ($row = 0; $row < 3; $row++) {
-for ($col = 0; $col < 3; $col++)
-echo " ".$a[$row][$col]+$b[$row][$col]." ";
-echo "<br/>";
-} }
-if($n===$p){
-echo " The multiplication of matrices: <br/>";
-$result=array();
-for ($i=0; $i < $m; $i++) {
-for($j=0; $j < $q; $j++){
-$result[$i][$j] = 0;
-for($k=0; $k < $n; $k++)
-$result[$i][$j] += $a[$i][$k] * $b[$k][$j];
-} }
-for ($row = 0; $row < $m; $row++)
+    echo "the first matrix :"."<br/>";
+    for ($row = 0; $row < $m; $row++) 
+    {
+        for ($col = 0; $col < $n; $col++)
+        echo " ".$a[$row][$col];
+        echo "<br/>";
+    }
+    echo "the second matrix :"."<br/>";
+        for ($row = 0; $row < $p; $row++)
+        {
+            for ($col = 0; $col < $q; $col++)
+            echo " ".$b[$row][$col];
+            echo "<br/>";
+        }
+        echo "the transpose for the first matrix is:"."<br/>";
+        for ($row = 0; $row < $m; $row++)
+        {
+            for ($col = 0; $col < $n; $col++)
+            echo " ".$a[$col][$row];
+            echo "<br/>";
+        }
+        if(($m===$p) and ($n===$q))
+        {
+            echo "the addition of matrices is:"."<br/>";
+            for ($row = 0; $row < 3; $row++) 
+            {
+            for ($col = 0; $col < 3; $col++)
+            echo " ".$a[$row][$col]+$b[$row][$col]." ";
+            echo "<br/>";
+             }          
+        }
+if($n===$p)
 {
-for ($col = 0; $col < $q; $col++)
-echo " ".$result[$row][$col];
-echo "<br/>";
-}
-}
-?>
+        echo " The multiplication of matrices: <br/>";
+        $result=array();
+        for ($i=0; $i < $m; $i++) 
+        {
+            for($j=0; $j < $q; $j++)
+            {
+                $result[$i][$j] = 0;
+                for($k=0; $k < $n; $k++)
+                $result[$i][$j] += $a[$i][$k] * $b[$k][$j];
+            } 
+        }
+    for ($row = 0; $row < $m; $row++)
+        {
+            for ($col = 0; $col < $q; $col++)
+            echo " ".$result[$row][$col];
+            echo "<br/>";
+        }   
+}?>
